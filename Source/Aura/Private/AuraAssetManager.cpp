998bbb946673409e2c12f0d39f85cc180033b044
newmri <newmri@naver.com>
2024-10-14 23:08:20 +0900

    AuraGameplayTags, AuraAssetManager 추가, 태그 코드로 변경

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,20 @@
+// Copyright Huibaekim
+
+
+#include "AuraAssetManager.h"
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine);
+
+	auto AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+}
---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,26 @@
+// Copyright Huibaekim
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	GameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Primary.Strength"));
+	GameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Primary.Intelligence"));
+	GameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Primary.Resilience"));
+	GameplayTags.Attributes_Primary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Primary.Vigor"));
+
+	GameplayTags.Attributes_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.MaxHealth"));
+	GameplayTags.Attributes_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.MaxMana"));
+	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.Armor"));
+	GameplayTags.Attributes_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.ArmorPenetration"));
+	GameplayTags.Attributes_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.BlockChance"));
+	GameplayTags.Attributes_Secondary_CriticalHitChange = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.CriticalHitChange"));
+	GameplayTags.Attributes_Secondary_CriticalHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.CriticalHitDamage"));
+	GameplayTags.Attributes_Secondary_CriticalHitResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.CriticalHitResistance"));
+	GameplayTags.Attributes_Secondary_HealthRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.HealthRegeneration"));
+	GameplayTags.Attributes_Secondary_ManaRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Atributes.Secondary.ManaRegeneration"));
+}
