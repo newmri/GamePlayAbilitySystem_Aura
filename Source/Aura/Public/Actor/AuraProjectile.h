f29f8c89084bdf5833da6bf2f68f6d174110ca62
newmri <newmri@naver.com>
2024-11-19 00:51:44 +0900

    Ranged Attack 애니메이션 재생

--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -21,6 +21,7 @@ class AURA_API AAuraProjectile : public AActor
 public:	
 	AAuraProjectile();
 
+	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
 
 	UPROPERTY(BlueprintReadWrite, meta = (ExposeOnSpawn = true))
