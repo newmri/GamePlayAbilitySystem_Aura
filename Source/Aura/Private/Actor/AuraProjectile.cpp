3fb8e9c1e0abccd728d0a4ad47850fdf610bc80a
newmri <newmri@naver.com>
2024-11-20 22:59:29 +0900

    샤먼 추가

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -60,7 +60,7 @@ void AAuraProjectile::Destroyed()
 
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (DamageEffectSpecHandle.Data.IsValid() && OtherActor == DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser())
+	if (!DamageEffectSpecHandle.Data.IsValid() || OtherActor == DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser())
 	{
 		return;
 	}
