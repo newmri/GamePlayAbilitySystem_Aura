cd41656bfcbaa59b66c46dc2d8b177c839ff0c99
newmri <newmri@naver.com>
2024-12-04 23:20:47 +0900

    AuraSummonAbility 작업중

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -0,0 +1,32 @@
+// Copyright Huibaekim
+
+
+#include "AbilitySystem/Abilities/AuraSummonAbility.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
+{
+	const auto Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const auto Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const auto DeltaSpread = SpawnSpread / NumMinions;
+
+	const auto LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
+	TArray<FVector> SpawnLocations;
+	for (int32 i = 0; i < NumMinions; ++i)
+	{
+		const auto Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+		auto ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit, ChosenSpawnLocation + FVector(0.f, 0.f, 400.f), ChosenSpawnLocation - FVector(0.f, 0.f, 400.f), ECC_Visibility);
+		if (Hit.bBlockingHit)
+		{
+			ChosenSpawnLocation = Hit.ImpactPoint;
+		}
+		
+		SpawnLocations.Add(ChosenSpawnLocation);
+	}
+	
+	return SpawnLocations;
+}
