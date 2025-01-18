ccac4a82e483eabf7ff0567e85ffb15166a86c36
newmri <newmri@naver.com>
2025-01-19 01:20:18 +0900

    레벨 UI 추가

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -23,6 +23,12 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 {
 	const auto AuraPlayerState = CastChecked<AAuraPlayerState>(PlayerState);
 	AuraPlayerState->OnXPChangedDelegate.AddUObject(this, &UOverlayWidgetController::OnXPChanged);
+	AuraPlayerState->OnLevelChangedDelegate.AddLambda(
+		[this](int32 NewLevel)
+		{
+			OnPlayerLevelChangedDelegate.Broadcast(NewLevel);
+		}
+	);
 	
 	const auto AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
 	
