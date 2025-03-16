21e617929578e20cc7b248fd846976baf5317cb8
newmri <newmri@naver.com>
2025-03-16 21:55:26 +0900

    Firebolt SpawnProjectiles 추가 디버그 화살 추가

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -2,7 +2,10 @@
 
 
 #include "AbilitySystem/Abilities/AuraFireBolt.h"
+
+#include "PrimitiveSceneInfo.h"
 #include "Aura/Public/AuraGameplayTags.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 
 FString UAuraFireBolt::GetDescription(int32 Level)
@@ -78,4 +81,43 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 		FMath::Min(Level, NumProjectiles),
 		ScaledDamage
 		);
+}
+
+void UAuraFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag,
+	bool bOverridePitch, float PitchOverride, AActor* HomingTarget)
+{
+	const auto bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+	if (false == bIsServer)
+	{
+		return;
+	}
+
+	if (auto CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo()))
+	{
+		const auto SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), SocketTag);
+		auto Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+		if (bOverridePitch)
+			Rotation.Pitch = PitchOverride;
+
+		const auto Forward = Rotation.Vector();
+		const auto LeftOfSpread = Forward.RotateAngleAxis(-ProjectileSpread / 2.f, FVector::UpVector);
+		const auto RightOfSpread = Forward.RotateAngleAxis(ProjectileSpread / 2.f, FVector::UpVector);
+		const auto Start = SocketLocation + FVector(0, 0, 5);
+
+		//const int32 NumOfProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+		const int32 NumOfProjectiles = NumProjectiles;
+		if (NumOfProjectiles > 1)
+		{
+			const auto DeltaSpread = ProjectileSpread / (NumOfProjectiles - 1);
+			for (int32 i = 0; i < NumOfProjectiles; ++i)
+			{
+				const auto Direction = LeftOfSpread.RotateAngleAxis(i * DeltaSpread, FVector::UpVector);
+				UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Start, Start + Direction * 75.f, 5, FLinearColor::White, 60, 1);
+			}
+		}
+		else
+		{
+			UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Start, Start + Forward * 75.f, 5, FLinearColor::White, 60, 1);
+		}
+	}
 }
