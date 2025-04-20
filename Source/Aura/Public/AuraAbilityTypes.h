0200f0546f0580363085c99dbb2cb20e2cae92f7
newmri <newmri@naver.com>
2025-04-21 00:11:31 +0900

    Radial Damage Param 추가

--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -59,6 +59,18 @@ struct FDamageEffectParams
 
 	UPROPERTY(BlueprintReadWrite)
 	FVector KnockbackForce = FVector::ZeroVector;
+	
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsRadialDamage = false;
+
+	UPROPERTY(BlueprintReadWrite)
+	float RadialDamageInnerRadius = 0.f;
+
+	UPROPERTY(BlueprintReadWrite)
+	float RadialDamageOuterRadius = 0.f;
+
+	UPROPERTY(BlueprintReadWrite)
+	FVector RadialDamageOrigin = FVector::ZeroVector;
 };
 
 USTRUCT(BlueprintType)
@@ -76,6 +88,10 @@ public:
 	TSharedPtr<FGameplayTag> GetDamageType() const {return DamageType;}
 	FVector GetDeathImpulse() const {return DeathImpulse;}
 	FVector GetKnockbackForce() const {return KnockbackForce;}
+	bool IsRadialDamage() const {return bIsRadialDamage;}
+	float GetRadialDamageInnerRadius() const {return RadialDamageInnerRadius;}
+	float GetRadialDamageOuteradius() const {return RadialDamageOuterRadius;}
+	FVector GetRadialDamageOrigin() const {return RadialDamageOrigin;}
 	
 	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
 	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
@@ -86,6 +102,10 @@ public:
 	void SetDamageType(TSharedPtr<FGameplayTag> InDamageType) { DamageType = InDamageType; }
 	void SetDeathImpulse(const FVector& InImpulse) { DeathImpulse = InImpulse; }
 	void SetKnockbackForce(const FVector& InForce){KnockbackForce = InForce;}
+	void SetIsRadialDamage(const bool bInIsRadialDamage) {bIsRadialDamage = bInIsRadialDamage;}
+	void SetRadialDamageInnerRadius(const float InRadialDamageInnerRadius){RadialDamageInnerRadius = InRadialDamageInnerRadius;}
+	void SetRadialDamageOuterRadius(const float InRadialDamageOuterRadius){RadialDamageOuterRadius = InRadialDamageOuterRadius;}
+	void SetRadialDamageOrigin(const FVector& InRadialDamageOrigin){RadialDamageOrigin = InRadialDamageOrigin;}
 	
 	virtual UScriptStruct* GetScriptStruct() const override
 	{
@@ -133,6 +153,18 @@ protected:
 
 	UPROPERTY()
 	FVector KnockbackForce = FVector::ZeroVector;
+
+	UPROPERTY()
+	bool bIsRadialDamage = false;
+
+	UPROPERTY()
+	float RadialDamageInnerRadius = 0.f;
+
+	UPROPERTY()
+	float RadialDamageOuterRadius = 0.f;
+
+	UPROPERTY()
+	FVector RadialDamageOrigin = FVector::ZeroVector;
 };
 
 template<>
